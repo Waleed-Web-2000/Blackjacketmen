9fe9753df8b51264eb1aef86d4ffcc9976991f41
Ecommerce Tracks <82581361+Waleed-Web-2000@users.noreply.github.com>
2024-02-24 21:44:05 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 // import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
 import axios from 'https://cdn.jsdelivr.net/npm/axios@1.3.5/+esm'
-// const instance = axios.create({
- // baseURL: 'https://onlineseller.pk/wp-json/wc/v3/',
+ const instance = axios.create({
+ baseURL: 'https://onlineseller.pk/wp-json/wc/v3/',
   // timeout: 1000,
- // withCredentials: true,
-//  auth: {
- //   username: "ck_de634d9199442a71813a2fbbda902cd05d907c12",
-  //  password: "cs_c2d3192a37386e6d91c05b57975ef6351da65ced"
- // }
-// });
+withCredentials: true,
+  auth: {
+   username: "ck_de634d9199442a71813a2fbbda902cd05d907c12",
+   password: "cs_c2d3192a37386e6d91c05b57975ef6351da65ced"
+ }
+});
 
-//instance.get('products')
- // .then((resp) => console.log(resp.data)) //
+instance.get('products')
+ .then((resp) => console.log(resp.data)) 
 
 Function FetchData() {
 	const [data, setData] = useState([])
