eb2af1bc9334da338ea6f3559cd6d9f7c35293f4
Ecommerce Tracks <82581361+Waleed-Web-2000@users.noreply.github.com>
2024-02-24 18:30:45 +0500

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ import axios from 'https://cdn.jsdelivr.net/npm/axios@1.3.5/+esm'
 const instance = axios.create({
   baseURL: 'https://onlineseller.pk/wp-json/wc/v3/',
   // timeout: 1000,
-  // withCredentials: true,
+  withCredentials: true,
+  Access-Control-Allow-Origin: 'https://blackjacketmen.vercel.app/',
   auth: {
     username: "ck_de634d9199442a71813a2fbbda902cd05d907c12",
     password: "cs_c2d3192a37386e6d91c05b57975ef6351da65ced"
