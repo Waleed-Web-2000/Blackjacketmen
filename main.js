e2e97042de4d84a6e1d873bb6057ed97a886a1a1
Ecommerce Tracks <82581361+Waleed-Web-2000@users.noreply.github.com>
2024-02-24 21:39:02 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
 // import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
 import axios from 'https://cdn.jsdelivr.net/npm/axios@1.3.5/+esm'
-const instance = axios.create({
-  baseURL: 'https://onlineseller.pk/wp-json/wc/v3/',
+// const instance = axios.create({
+ // baseURL: 'https://onlineseller.pk/wp-json/wc/v3/',
   // timeout: 1000,
  // withCredentials: true,
-  auth: {
-    username: "ck_de634d9199442a71813a2fbbda902cd05d907c12",
-    password: "cs_c2d3192a37386e6d91c05b57975ef6351da65ced"
-  }
-});
+//  auth: {
+ //   username: "ck_de634d9199442a71813a2fbbda902cd05d907c12",
+  //  password: "cs_c2d3192a37386e6d91c05b57975ef6351da65ced"
+ // }
+// });
 
-instance.get('products')
-  .then((resp) => console.log(resp.data))
+//instance.get('products')
+ // .then((resp) => console.log(resp.data)) //
+
+Function FetchData() {
+	const [data, setData] = useState([])
+	useEffect(()=>{
+		axios.get('https://onlineseller.pk/wp-json/wc/v3/')
+		.then(res => console.log(res))
+		.catch(err => console.log(err));
+	}, [])
+	return (
+			<div className='container'>
+			<div className='mt-3'>
+				<h3>FetchData</h3>
+			</div>
+			</div>
+		)
+}
 
 
 // const api = new WooCommerceRestApi({
