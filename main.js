a79df3faa72b2e3d001670d531d0783bf5a3e8d8
Ecommerce Tracks <82581361+Waleed-Web-2000@users.noreply.github.com>
2024-02-24 15:28:20 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+// import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
+import axios from 'axios'
+
+const instance = axios.create({
+  baseURL: 'https://onlineseller.pk/wp-json/wc/v3/',
+  // timeout: 1000,
+  // withCredentials: true,
+  auth: {
+    username: "ck_de634d9199442a71813a2fbbda902cd05d907c12",
+    password: "cs_c2d3192a37386e6d91c05b57975ef6351da65ced"
+  }
+});
+
+instance.get('products')
+  .then((resp) => console.log(resp.data))
+
+// const api = new WooCommerceRestApi({
+//   url: "https://onlineseller.pk/",
+//   consumerKey: "ck_de634d9199442a71813a2fbbda902cd05d907c12",
+//   consumerSecret: "cs_c2d3192a37386e6d91c05b57975ef6351da65ced",
+//   version: "wc/v3"
+// });
+
+// api.get("products")
+//   .then((response) => {
+//     console.log(response.data);
+//   })
+//   .catch((error) => {
+//     console.log(error.response.data);
+//   });
