c7d8cd82cdfcd51f31389f99dbd6014801af5b73
Ecommerce Tracks <82581361+Waleed-Web-2000@users.noreply.github.com>
2024-02-26 15:29:07 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 
 
  const instance = axios.create({
- baseURL: 'https://onlineseller.pk/wp-json/wc/v3/',
+ baseURL: 'http://leatherjackt.com/wp-json/wc/v3/products?ck_3af00de51b90e9a3315e7e39d75197b534d11a53=cs_abb30dcb605f6ce1e322e2260bca513ea051c563',
   // timeout: 1000,
   // withCredentials: true,
   auth: {
-   username: "ck_de634d9199442a71813a2fbbda902cd05d907c12",
-   password: "cs_c2d3192a37386e6d91c05b57975ef6351da65ced"
+   username: "ck_3af00de51b90e9a3315e7e39d75197b534d11a53",
+   password: "cs_abb30dcb605f6ce1e322e2260bca513ea051c563"
  }
 });
 instance.get('products')
