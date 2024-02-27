3b16e3ace10e70f9fcfea581686cb87dcb4d2417
Ecommerce Tracks <82581361+Waleed-Web-2000@users.noreply.github.com>
2024-02-27 18:58:44 +0500

    Update main.js

--- main.js
+++ main.js
@@ -5,8 +5,7 @@
 				console.log(objectData[0].title);
 				let tableData="";
 				objectData.map((values) => {
-				  tableData+=`<tr><td>${values.id}</td><td>${values.name}</td><td>${values.price}</td><td>${values.images}</td></tr>`;
-				  JSON.stringify(values.images)
+				  tableData+=`<tr><td>${values.id}</td><td>${values.name}</td><td>${values.price}</td><td>${values.images(src)}</td></tr>`;
 				});
 				document.getElementById("table_body").innerHTML=tableData;
 		})
