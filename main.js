bc5e42f81a93c3fb15f48c49d346795eb489a28d
Ecommerce Tracks <82581361+Waleed-Web-2000@users.noreply.github.com>
2024-02-27 18:01:44 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-fetch("https://black-leather-jacket-mens.leatherjackt.com/response.json")
-		.then(response => response.json())
-		.then(res => {
-				const data = res.products;
-				let rows = '';
-				data.forEach(product => {
-						rows += `<tr><td>${product.id}</td><td>${product.name}</td><td>${product.description}</td></tr>`
-				})
-			        console.log(rows)
-				document.getElementById(`tableRows`).innerHTML = rows;
+<script>
+		fetch("https://black-leather-jacket-mens.leatherjackt.com/response.json").then((data) => {
+				return data.json();
+		}).then((objectData) => {
+				console.log(objectData[0].title);
+				let tableData="";
+				objectData.map((values) => {
+				  tableData+=`<tr><td>${values.title}</td><td>${values.description}</td><td>${values.price}</td></tr>`;
+				});
+				document.getElementById("table_body").innerHTML=tableData;
 		})
-		.catch(error => console.log(error))
+		
